c48474e5397f1405ad1093ec25ecb2094b03a928
RvN-R <rvannairn@googlemail.com>
2021-08-27 16:29:31 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hpq+@aiu&wu0)41$w02u=489t-$!c-6$jdda96=c1lm7+$0j)9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rvn-django-todo-app.herokuapp.com']
 
 
 # Application definition
